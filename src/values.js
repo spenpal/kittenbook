147306c32037901799ae1a274b702b9e0c92205c
spenpal2000 <sanjeev.penupala@gmail.com>
2021-02-16 21:01:13 -0600

    feat: capture state of program

---
+++ src/values.js
@@ -0,0 +1,9 @@
+let projectName = 'kittenbook';
+let versionNumber = '0.0.1';
+let currentDate = new Date();
+let currentTime = currentDate.getFullYear() + '-' + 
+                    (currentDate.getMonth() + 1) + '-' + 
+                    currentDate.getDate() + ' at ' +
+                    currentDate.getHours() + ':' +
+                    currentDate.getMinutes() + ':' +
+                    currentDate.getSeconds();
