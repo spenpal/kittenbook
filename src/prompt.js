ade562e4cceb68567cfe9a7044857cc8b96b3544
spenpal2000 <sanjeev.penupala@gmail.com>
2021-02-16 21:01:26 -0600

    feat: add state variables to program

---
+++ src/prompt.js
@@ -0,0 +1,5 @@
+// alert('Hello, Sanjeev!')
+let userName = prompt("Hello, what's your name?")
+document.body.innerHTML = '<h1>Hello, ' + userName + '!</h1>' + 
+                            '<p>' + projectName + ' ' + versionNumber + 
+                            ' accessed on: ' + currentTime + '</p>';
