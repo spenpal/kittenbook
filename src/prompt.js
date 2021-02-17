0c85c349bebb467181199c0b56ab5cd233f281d1
spenpal2000 <sanjeev.penupala@gmail.com>
2021-02-16 23:49:29 -0600

    chore: remove redunant comments

--- src/prompt.js
+++ src/prompt.js
@@ -1,4 +1,3 @@
-// alert('Hello, Sanjeev!')
 let userName = prompt("Hello, what's your name?")
 document.body.innerHTML = '<h1>Hello, ' + userName + '!</h1>' + 
                             '<p>' + projectName + ' ' + versionNumber + 
